29cab32e74f7f100f9f6adcad6c78c60e9dc25d4
Przemyslaw Tomaszewski <przemyslaw.tomaszewski@payu.pl>
2015-04-14 08:43:35 +0200

    Removed doubled invocation of main gateway class

--- woocommerce-payu-payment-gateway.php
+++ woocommerce-payu-payment-gateway.php
@@ -9,23 +9,23 @@
  * License: LGPL 3.0
  */
 
-add_action( 'plugins_loaded', 'bpmj_woocommerce_payu_init', 0 );
-function bpmj_woocommerce_payu_init()
+add_action('plugins_loaded', 'woocommerce_payu_init', 0);
+function woocommerce_payu_init()
 {
     // Sprawdzenie, czy wtyczka WooCommerce jest aktywowana
-    if ( ! class_exists( 'WC_Payment_Gateway' ) ) return;
+    if (!class_exists( 'WC_Payment_Gateway')) return;
 
     // Wczytanie klasy odpowiedzialnej za obsługę płatności
-    include_once( 'includes/class-woocommerce-payu.php' );
+    include_once('includes/class-woocommerce-payu.php');
 
     // Dodanie bramki do WooCommerce
-    add_filter( 'woocommerce_payment_gateways', 'bpmj_woocommerce_payu_gateway' );
-    function bpmj_woocommerce_payu_gateway( $methods ) {
+    add_filter('woocommerce_payment_gateways', 'bpmj_woocommerce_payu_gateway');
+
+    function bpmj_woocommerce_payu_gateway($methods) {
         $methods[] = 'BPMJ_WooCommerce_PayU';
+
         return $methods;
     }
-
-    new BPMJ_WooCommerce_PayU();
 }
 
 ?>
