ab492d919c6d9955698705b0e6edb533719de9e8
Tomasz Regdos <tomasz.regdos@payu.pl>
2017-01-11 12:08:04 +0100

    Cleanup code, remove validity time

--- includes/form-fields.php
+++ includes/form-fields.php
@@ -2,14 +2,14 @@
 
 return array(
     'enabled' => array(
-        'title'=> __('Enable:', 'payu'),
+        'title' => __('Enable:', 'payu'),
         'type' => 'checkbox',
         'label' => ' ',
         'default' => 'no'
     ),
     'title' => array(
         'title' => __('Title:', 'payu'),
-        'type'=> 'text',
+        'type' => 'text',
         'description' => __('Title of PayU Payment Gateway that users sees on Checkout page.', 'payu'),
         'default' => __('PayU', 'payu'),
         'desc_tip' => true
@@ -23,19 +23,19 @@ return array(
     'md5' => array(
         'title' => __('Second key (MD5):', 'payu'),
         'type' => 'text',
-        'description' =>  __('Second key from "Configuration Keys" section of PayU management panel.', 'payu'),
+        'description' => __('Second key from "Configuration Keys" section of PayU management panel.', 'payu'),
         'desc_tip' => true
     ),
     'client_id' => array(
         'title' => __('OAuth - client_id:', 'payu'),
         'type' => 'text',
-        'description' =>  __('Client Id for OAuth identifier  from "Configuration Keys" section of PayU management panel.', 'payu'),
+        'description' => __('Client Id for OAuth identifier  from "Configuration Keys" section of PayU management panel.', 'payu'),
         'desc_tip' => true
     ),
     'client_secret' => array(
         'title' => __('OAuth - client_secret:', 'payu'),
         'type' => 'text',
-        'description' =>  __('First key from "Configuration Keys" section of PayU management panel.', 'payu'),
+        'description' => __('First key from "Configuration Keys" section of PayU management panel.', 'payu'),
         'desc_tip' => true
     ),
     'description' => array(
@@ -45,17 +45,10 @@ return array(
         'default' => __('PayU is a leading payment services provider with presence in 16 growth markets across the world.', 'payu'),
         'desc_tip' => true
     ),
-    'validity_time' => array(
-        'title' => __('Validity time:', 'payu'),
-        'type' => 'text',
-        'description' =>  __('Time when paying for order is possible (in seconds).', 'payu'),
-        'default' => '1440',
-        'desc_tip' => true
-    ),
     'payu_feedback' => array(
-        'title'=> __('Automatic collection:', 'payu'),
+        'title' => __('Automatic collection:', 'payu'),
         'type' => 'checkbox',
-        'description' =>  __('Automatic collection makes it possible to automatically confirm incoming payments.', 'payu'),
+        'description' => __('Automatic collection makes it possible to automatically confirm incoming payments.', 'payu'),
         'label' => ' ',
         'default' => 'no',
         'desc_tip' => true
--- woocommerce-gateway-payu.php
+++ woocommerce-gateway-payu.php
@@ -3,7 +3,7 @@
  * Plugin Name: PayU Payment Gateway
  * Plugin URI: https://github.com/PayU/plugin_woocommerce
  * Description: PayU payment gateway for WooCommerce
- * Version: 1.1.1
+ * Version: 1.2.0
  * Author: PayU SA
  * Author URI: http://www.payu.com
  * License: LGPL 3.0
@@ -13,28 +13,20 @@
 
 add_action('plugins_loaded', 'woocommerce_payu_init', 0);
 
-add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
-
-function woocommerce_payu_init() {
+function woocommerce_payu_init()
+{
     if (!class_exists('WC_Payment_Gateway')) return;
 
-    load_plugin_textdomain( 'payu', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
+    load_plugin_textdomain('payu', false, dirname(plugin_basename(__FILE__)) . '/lang/');
 
     include_once('includes/class-woocommerce-payu.php');
 
     add_filter('woocommerce_payment_gateways', 'woocommerce_payu_add_gateway');
 }
 
-function woocommerce_payu_add_gateway($methods) {
+function woocommerce_payu_add_gateway($methods)
+{
     $methods[] = 'WC_Gateway_PayU';
 
     return $methods;
-}
-
-function add_theme_scripts()
-{
-
-    wp_enqueue_style('PayUMethodsCss', plugin_dir_url( __FILE__ ).'assets/css/PayUMethodsCss.css',array(), '1.1', 'all');
-
-    wp_enqueue_script('PayUMethodsJs',  plugin_dir_url( __FILE__ ).'assets/js/PayUMethodsJs.js');
 }
