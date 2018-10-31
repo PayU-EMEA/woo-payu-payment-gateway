dd0c7d07254a975761b848111f4389e153d96e1b
Tomasz Regdos <tomasz.regdos@payu.pl>
2018-10-31 13:23:45 +0100

    Version update

--- includes/class-woocommerce-payu.php
+++ includes/class-woocommerce-payu.php
@@ -5,7 +5,7 @@ require_once 'OauthCacheWP.php';
 
 class WC_Gateway_PayU extends WC_Payment_Gateway
 {
-    private $pluginVersion = '1.2.7';
+    private $pluginVersion = '1.2.8';
 
     private $payu_feedback;
     private $sandbox;
