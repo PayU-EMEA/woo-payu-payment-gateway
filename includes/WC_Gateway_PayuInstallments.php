06ad7174fdf6b0d91934eede2854af05e4098791
Filip Rytwiński <filip.rytwinski@payu.com>
2023-01-09 15:14:01 +0100

    [PCE-1441] fix installment payment method in email communication

--- includes/WC_Gateway_PayuInstallments.php
+++ includes/WC_Gateway_PayuInstallments.php
@@ -14,25 +14,20 @@ class WC_Gateway_PayuInstallments extends WC_PayUGateways
                     add_filter('woocommerce_available_payment_gateways', [$this, 'unset_gateway']);
                 } else {
                     wp_enqueue_style('payu-installments-widget', plugins_url( '/assets/css/payu-installments-widget.css', PAYU_PLUGIN_FILE ), [], PAYU_PLUGIN_VERSION);
-                    add_filter( 'woocommerce_gateway_title', [ $this, 'installments_filter_gateway_title' ], 10, 2);
+                    add_filter('woocommerce_gateway_title', [$this, 'installments_filter_gateway_title'], 10, 2);
+                    add_filter('woocommerce_gateway_description', [$this, 'installments_filter_gateway_title_description'], 10, 2);
                 }
             }
         }
     }
 
-    public function installments_filter_gateway_title( $title, $id )
-    {
-        if ($id === 'payuinstallments' &&
-            get_option('woocommerce_payuinstallments_settings')['credit_widget_on_checkout_page'] === 'yes' &&
-            get_woocommerce_currency() === 'PLN') {
-            wp_enqueue_script('payu-installments-widget', 'https://static.payu.com/res/v2/widget-mini-installments.js', [], PAYU_PLUGIN_VERSION);
-
+    public function installments_filter_gateway_title_description($description, $id) {
+        if ($this->should_display_installments_widget($id)) {
             $posId = $this->pos_id;
             $widgetKey = $this->pos_widget_key;
             $priceTotal = WC()->cart->total;
-            $transformedTitle =
-                '<div class="wc-payu-installments-widget-cart">'.$title.
-                '<div id="installment-mini-cart"></div>'.
+            $transformedDescription =
+                $description.
                 '<script type="text/javascript">'.
                 'function showInstallmentsWidget() {'.
                 '   if(window.OpenPayU && document.getElementById(\'installment-mini-cart\').childNodes.length === 0) {'.
@@ -48,12 +43,33 @@ class WC_Gateway_PayuInstallments extends WC_PayUGateways
                 '}'.
                 'document.addEventListener("DOMContentLoaded", showInstallmentsWidget);'.
                 'showInstallmentsWidget();'.
-                '</script>'.
+                '</script>';
+            return $transformedDescription;
+        }
+
+        return $description;
+    }
+
+    public function installments_filter_gateway_title($title, $id)
+    {
+        if ($this->should_display_installments_widget($id)) {
+            wp_enqueue_script('payu-installments-widget', 'https://static.payu.com/res/v2/widget-mini-installments.js', [], PAYU_PLUGIN_VERSION);
+
+            $transformedTitle =
+                '<div class="wc-payu-installments-widget-cart">'.$title.
+                '<div id="installment-mini-cart"></div>'.
                 '</div>';
             return $transformedTitle;
         }
 
         return $title;
+    }
+
+    function should_display_installments_widget($id)
+    {
+        return $id === 'payuinstallments' &&
+            get_option('woocommerce_payuinstallments_settings')['credit_widget_on_checkout_page'] === 'yes' &&
+            get_woocommerce_currency() === 'PLN';
     }
 
     public function payment_fields()
