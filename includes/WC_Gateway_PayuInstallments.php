4928ac667ad8ae0e8ed8b0d713aac014b0fabc3c
Filip Rytwiński <filip.rytwinski@payu.com>
2023-01-13 15:25:11 +0100

    [PCE-1441] fix installment payment method caption on summary page

--- includes/WC_Gateway_PayuInstallments.php
+++ includes/WC_Gateway_PayuInstallments.php
@@ -14,13 +14,13 @@ class WC_Gateway_PayuInstallments extends WC_PayUGateways
                     add_filter('woocommerce_available_payment_gateways', [$this, 'unset_gateway']);
                 } else {
                     wp_enqueue_style('payu-installments-widget', plugins_url( '/assets/css/payu-installments-widget.css', PAYU_PLUGIN_FILE ), [], PAYU_PLUGIN_VERSION);
-                    add_filter('woocommerce_gateway_title', [$this, 'installments_filter_gateway_title'], 10, 2);
+                    add_filter('woocommerce_gateway_description', [$this, 'installments_filter_gateway_description'], 10, 2);
                 }
             }
         }
     }
 
-    public function installments_filter_gateway_title($title, $id)
+    public function installments_filter_gateway_description($description, $id)
     {
         if ($this->should_display_installments_widget($id)) {
             wp_enqueue_script('payu-installments-widget', 'https://static.payu.com/res/v2/widget-mini-installments.js', [], PAYU_PLUGIN_VERSION);
@@ -28,18 +28,19 @@ class WC_Gateway_PayuInstallments extends WC_PayUGateways
             $posId = $this->pos_id;
             $widgetKey = $this->pos_widget_key;
             $priceTotal = WC()->cart->total;
-            $transformedTitle =
-                '<div class="wc-payu-installments-widget-cart">'.$title.
-                '<div id="installment-mini-cart"'.
-                ' data-pos-id="'.$posId.'"'.
-                ' data-widget-key="'.$widgetKey.'"'.
-                ' data-price-total="'.$priceTotal.'">'.
-                '</div>'.
-                '</div>';
-            return $transformedTitle;
+            $transformedDescription =
+                $description.
+                '<script type="text/javascript">'.
+                'var PayUInstallmentsWidgetCartData = {'.
+                '	priceTotal: '.$priceTotal.','.
+                '   posId: \''.$posId.'\','.
+                '   widgetKey: \''.$widgetKey.'\''.
+                '}'.
+                '</script>';
+            return $transformedDescription;
         }
 
-        return $title;
+        return $description;
     }
 
     function should_display_installments_widget($id)
