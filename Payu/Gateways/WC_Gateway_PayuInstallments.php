6e5e3acae3d22fd5e9cf3a2f8227804c54c2933a
kacper.melon <kacper.melon@payu.com>
2025-05-28 12:20:39 +0200

    [PCE-4185] remove displaying widget next to installments gateway in checkout (classic shortcode)

--- Payu/Gateways/WC_Gateway_PayuInstallments.php
+++ Payu/Gateways/WC_Gateway_PayuInstallments.php
@@ -33,32 +33,6 @@ class WC_Gateway_PayuInstallments extends WC_Payu_Gateways {
 		];
 	}
 
-	public function get_description(): string {
-		wp_enqueue_style( 'payu-installments-widget', plugins_url( '/assets/css/payu-installments-widget.css', PAYU_PLUGIN_FILE ), [], PAYU_PLUGIN_VERSION );
-
-		wp_enqueue_script( 'payu-installments-widget-checkout', plugins_url( '/assets/js/payu-installments-widget-checkout.js', PAYU_PLUGIN_FILE ), [
-			'jquery',
-			'payu-installments-widget'
-		], PAYU_PLUGIN_VERSION );
-		$posId      = $this->pos_id;
-		$widgetKey  = $this->pos_widget_key;
-		$excludedPaytypes  = $this->get_credit_widget_excluded_paytypes();
-        $currency = get_woocommerce_currency();
-		$priceTotal = WC()->cart->get_total( '' );
-
-		return
-			$this->description .
-			'<script type="text/javascript">' .
-			'var PayUInstallmentsWidgetCartData = {' .
-			'	priceTotal: ' . $priceTotal . ',' .
-			'   posId: \'' . $posId . '\',' .
-			'   widgetKey: \'' . $widgetKey . '\'' .
-            '   excludedPaytypes: \'' . $excludedPaytypes . '\'' . // nie wiadomo czy działa
-            '   currencySign: \'' . $currency . '\'' .
-			'}' .
-			'</script>';
-	}
-
     private function widget_on_checkout_enabled() {
         $payuSettings = get_option('payu_settings_option_name');
         if ( ! empty($payuSettings) && isset($payuSettings['credit_widget_on_checkout_page']) ) {
