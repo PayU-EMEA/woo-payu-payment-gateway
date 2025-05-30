bb27e652e27283ecf9d0f74c57c5874dbc66ea1e
kacper.melon <kacper.melon@payu.com>
2025-05-30 12:14:44 +0200

    [PCE-4185] remove displaying widget next to installments gateway in checkout (blocks template)

--- Payu/Gateways/WC_Gateway_PayuInstallments.php
+++ Payu/Gateways/WC_Gateway_PayuInstallments.php
@@ -13,46 +13,6 @@ class WC_Gateway_PayuInstallments extends WC_Payu_Gateways {
 		if ( ! $this->try_retrieve_banks() || ! parent::is_available() ) {
 			return false;
 		}
-
-		if ( $this->widget_on_checkout_enabled() && $this->is_checkout_page() ) {
-			wp_enqueue_script( 'payu-installments-widget', 'https://static.payu.com/res/v2/widget-mini-installments.js', [], PAYU_PLUGIN_VERSION );
-		}
-
 		return true;
 	}
-
-	// Additional data for Blocks
-	public function get_additional_data(): array {
-		return [
-			'widgetOnCheckout' => $this->widget_on_checkout_enabled(),
-			'posId'            => $this->pos_id,
-			'widgetKey'        => $this->pos_widget_key,
-			'excludedPaytypes' => $this->get_credit_widget_excluded_paytypes(),
-			'lang'             => $this->getLanguage(),
-            'currency'         => get_woocommerce_currency(),
-			'total'            => $this->getTotal()
-		];
-	}
-
-    private function widget_on_checkout_enabled() {
-        $payuSettings = get_option('payu_settings_option_name');
-        if ( ! empty($payuSettings) && isset($payuSettings['credit_widget_on_checkout_page']) ) {
-            return $payuSettings['credit_widget_on_checkout_page'] === 'yes';
-        } else {
-            return false;
-        }
-    }
-
-	private function is_checkout_page(): bool {
-		return is_checkout() || has_block( 'woocommerce/checkout' );
-	}
-
-    private function get_credit_widget_excluded_paytypes(): array {
-        $payuSettings = get_option('payu_settings_option_name');
-        if ( ! empty($payuSettings) && isset($payuSettings['credit_widget_excluded_paytypes']) ) {
-            return $payuSettings['credit_widget_excluded_paytypes'];
-        } else {
-            return [];
-        }
-    }
 }
--- src/js/payuinstallments.js
+++ src/js/payuinstallments.js
@@ -1,7 +1,6 @@
 import { decodeEntities } from '@wordpress/html-entities';
 import { getSetting } from '@woocommerce/settings';
 import { registerPaymentMethod } from '@woocommerce/blocks-registry';
-import { useEffect } from '@wordpress/element';
 
 const name = 'payuinstallments';
 
@@ -11,14 +10,6 @@ const available = decodeEntities( settings.available ) || false;
 const title = decodeEntities( settings.title ) || 'PayU';
 const description = decodeEntities( settings.description ) || '';
 const iconUrl = settings.icon;
-const posId = decodeEntities( settings.additionalData?.posId ) || '';
-const widgetKey = decodeEntities( settings.additionalData?.widgetKey ) || '';
-const excludedPaytypes = decodeEntities( settings.additionalData?.excludedPaytypes ) || '[]';
-const lang = decodeEntities( settings.additionalData?.lang ) || 'en';
-const currency = decodeEntities( settings.additionalData?.currency) || '';
-const total = decodeEntities( settings.additionalData?.total ) || '';
-const widgetOnCheckout =
-  decodeEntities( settings.additionalData?.widgetOnCheckout ) || false;
 
 const canMakePayment = () => {
   return available;
@@ -31,29 +22,7 @@ const Content = () => {
 const Label = ( props ) => {
   const { PaymentMethodLabel } = props.components;
 
-  useEffect( () => {
-    window.OpenPayU?.Installments?.miniInstallment( '#installment-mini-cart', {
-      creditAmount: Number( total ),
-      posId,
-      key: widgetKey,
-      excludedPaytypes: excludedPaytypes,
-      lang: lang,
-      currencySign: currency,
-      showLongDescription: true,
-    } );
-  }, [] );
-
-  return widgetOnCheckout && posId && widgetKey && total ? (
-    <div className="payu-block-installments-label">
-      <div>
-        <PaymentMethodLabel text={ title } className="payu-block-method" />
-        <span className="payu-block-method-logo">
-          <img src={ iconUrl } alt="PayU" name={ title } />
-        </span>
-      </div>
-      <div id="installment-mini-cart"></div>
-    </div>
-  ) : (
+  return (
     <>
       <PaymentMethodLabel text={ title } className="payu-block-method" />
       <span className="payu-block-method-logo">
