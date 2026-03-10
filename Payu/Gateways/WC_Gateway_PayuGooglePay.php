3f4d29e0ce2b95c70b6748ce0805f3f5b4ff26a5
Antoni Kakolewski <antonikakolewski0@gmail.com>
2026-03-10 14:16:38 +0100

    Google Pay on classic checkout

---
+++ Payu/Gateways/WC_Gateway_PayuGooglePay.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Payu\PaymentGateway\Gateways;
+
+class WC_Gateway_PayuGooglePay extends WC_Payu_Gateways
+{
+    protected string $paytype = 'ap';
+
+    function __construct()
+    {
+        parent::__construct('payugooglepay');
+
+        if ($this->is_enabled()) {
+            $this->icon = apply_filters('woocommerce_payu_icon', plugins_url('/assets/images/ap.svg', PAYU_PLUGIN_FILE));
+            add_action('wp_enqueue_scripts', [$this, 'include_scripts']);
+        }
+    }
+
+    public function is_available(): bool
+    {
+        if (! $this->try_retrieve_banks()) {
+            return false;
+        }
+
+        return parent::is_available();
+    }
+
+    public function payment_fields(): void
+    {
+        ?>
+        <ul class="payu-google-pay-error woocommerce-error" role="alert">
+            <li><?php esc_html_e('This payment method is not available.', 'woo-payu-payment-gateway') ?></li>
+        </ul>
+        <?php
+        parent::payment_fields();
+        ?>
+        <script>
+            var payuGooglePayConfig = {
+                currency: "<?php echo esc_attr(get_woocommerce_currency()) ?>",
+                posId: "<?php echo esc_attr($this->pos_id) ?>",
+                totalPrice: "<?php echo esc_attr(WC()->cart->get_total('')) ?>",
+                env: "<?php echo $this->sandbox ? 'TEST' : 'PROD' ?>"
+            }
+        </script>
+        <input type="hidden" name="payu-google-token" id="payu-google-token" value="" />
+        <?php
+        $this->agreements_field();
+    }
+
+    public function include_scripts(): void
+    {
+        wp_enqueue_script('google-pay', "https://pay.google.com/gp/p/js/pay.js", [], null);
+    }
+
+    protected function get_payu_pay_method(): array
+    {
+        $token = sanitize_text_field($_POST['payu-google-token']);
+
+        return [
+            'payMethod' => [
+                'type'  => 'PBL',
+                'value' => 'ap',
+                'authorizationCode' => $token
+            ]
+        ];
+
+        return $this->get_payu_pay_method_array('CARD_TOKEN', $token);
+    }
+}
